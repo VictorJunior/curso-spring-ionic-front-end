ded65b7a89a2b8653d3c8961c83f5e74ec8a3694
Victor Junior <j2system.victor@gmail.com>
2018-12-05 20:53:47 -0200

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -18,7 +18,9 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
-   constructor(
+  codpedido: string;
+
+  constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public clienteService: ClienteService,
@@ -52,12 +54,16 @@ export class OrderConfirmationPage {
   back() {
     this.navCtrl.setRoot('CartPage');
   }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
    
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403) {
@@ -65,4 +71,10 @@ export class OrderConfirmationPage {
         }
       });
   }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
+  
 }
