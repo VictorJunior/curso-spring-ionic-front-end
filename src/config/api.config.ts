b321d61992047fe8ec457faff0b4e88244fd9b8d
Victor Junior <j2system.victor@gmail.com>
2019-01-26 17:17:08 -0200

    Bugfix - cancelamento da camera ou galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "https://curso-spring-ionic-j2.herokuapp.com/",
+    baseUrl: "https://curso-spring-ionic-j2.herokuapp.com",
+    /*baseUrl: "http://localhost:8090",*/
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-victor"
-}
+} 
